8ee545cc6c71bcd316449aee94c2b21a1d8487a0
adnan98 <adnan98@gitlab.com>
2019-06-26 15:10:04 +0700

    update5

--- order.cpp
+++ order.cpp
@@ -16,6 +16,16 @@ int awal=-1, akhir=-1;
 
 order queueOrder[MAX];
 
+void addData(string Pesanan,int Kuantitas,string Adress,string Jasa)
+{
+	++akhir; //menambah nilai Akhir dengan 1
+	queueOrder[akhir].idOrder	= to_string(1000+akhir);
+	queueOrder[akhir].barang	= Pesanan ;
+	queueOrder[akhir].jumlah	= Kuantitas;
+	queueOrder[akhir].alamat	= Adress;
+	queueOrder[akhir].jasaKirim	= Jasa;	
+}
+
 void init(){
     ++awal;
     ++akhir;
@@ -40,36 +50,6 @@ void init(){
 	queueOrder[akhir].jasaKirim	    = "POS";
 }
 
-void tampil(){
-	system("CLS");
-	cout<<"Id Order\tNamaBarang\tJumlah\tAlamat\tJasa Kirim\n";
-	for(int i=awal; i<=akhir; i++){
-		cout<<queueOrder[i].idOrder<<"\t\t"<<queueOrder[i].barang<<"\t"<<queueOrder[i].jumlah<<"\t"<<queueOrder[i].alamat<<"\t"<<queueOrder[i].jasaKirim<<"\n";
-	}
-}
-
-
-//MAHESA
-void tambah(){//inQueue : Menambah item di akhir index
-
-}
-
-//HADI
-void urut(){
-	
-}
-
-//MITHA & ISQIA
-void cari(){
-	
-}
-
-//FAQIH
-void proses(){//deQueeu : Mengambilitem di awal index
-	
-}
-
-
 bool full(){
 	if(akhir==MAX-1){
 		return true;
@@ -84,6 +64,79 @@ bool empty(){
 	}else{
 		return false;
 	}
+}
+
+void tampil(){
+	system("CLS");
+	if(!empty()){
+		cout<<"Id Order\tNamaBarang\tJumlah\tAlamat\tJasa Kirim\n";
+		for(int i=awal; i<=akhir; i++){
+			cout<<queueOrder[i].idOrder<<"\t\t"<<queueOrder[i].barang<<"\t"<<queueOrder[i].jumlah<<"\t"<<queueOrder[i].alamat<<"\t"<<queueOrder[i].jasaKirim<<"\n";
+		}
+	}else{
+		cout<<"DATA KOSONG\n";
+	}
+	
+}
+
+//MAHESA
+void tambah(){
+	//inQueue : Menambah item di akhir index
+	if (full()){
+		cout<<"Maaf Pemesanan Kami Penuh,,Silahkan Order dilain Waktu";
+	}else{
+		int Kuantitas;
+		string Adress;
+		char Pesanan[30];
+		string Jasa;
+		
+		cout<<" - Silahkan masukan nama barang pesanan : ";
+		//cin>>Pesanan;
+		cin.getline(Pesanan,sizeof(Pesanan));
+		cout<<" - Jumlah barang yang akan dipesan : ";
+		cin>>Kuantitas;
+		cout<<" - Alamat anda : ";
+		cin>>Adress;
+		cout<<" - Penggunaan Jasa Kirim : ";
+		cin>>Jasa;
+		
+		addData(Pesanan,Kuantitas,Adress,Jasa);
+		
+		cout<<"Data Berhasil Masuk";
+	}	
+}
+
+//HADI
+void urut(){
+	
+}
+
+//MITHA & ISQIA
+void cari(){
+	
+}
+
+//FAQIH
+void proses(){//deQueeu : Mengambilitem di awal index
+	system("CLS");
+
+	if(!empty()){
+		cout<<"Silahkan Proses Data Berikut...."<<endl;
+		cout<<"Id Order\tNamaBarang\tJumlah\tAlamat\tJasa Kirim\n";
+		cout<<queueOrder[0].idOrder<<"\t\t"<<queueOrder[0].barang<<"\t"<<queueOrder[0].jumlah<<"\t"<<queueOrder[0].alamat<<"\t"<<queueOrder[0].jasaKirim<<"\n";
+		cout<<endl;
+		
+		for(int i=awal;i<akhir;i++){
+			queueOrder[i].idOrder=queueOrder[i+1].idOrder;
+			queueOrder[i].alamat=queueOrder[i+1].alamat;
+			queueOrder[i].barang=queueOrder[i+1].barang;
+			queueOrder[i].jasaKirim=queueOrder[i+1].jasaKirim;
+			queueOrder[i].jumlah=queueOrder[i+1].jumlah;
+		}
+		akhir--;
+	}else{
+		cout<<"Antrean kosong\n";
+	}	
 }
 
 void menu(){
